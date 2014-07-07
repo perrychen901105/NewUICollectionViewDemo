a6c665aaaec7d772ba5ccde350bda60e8eac3a52
Perry <chenzhipeng901105@gmail.com>
2014-07-07 18:09:32 +0800

    add new flickr

--- Flickr Search/Flickr Search/ViewController.m
+++ Flickr Search/Flickr Search/ViewController.m
@@ -7,12 +7,20 @@
 //
 
 #import "ViewController.h"
+#import "Flickr.h"
+#import "FlickrPhoto.h"
+
 
 @interface ViewController () <UITextFieldDelegate>
 
 @property(nonatomic, weak) IBOutlet UIToolbar *toolbar;
 @property(nonatomic, weak) IBOutlet UIBarButtonItem *shareButton;
 @property(nonatomic, weak) IBOutlet UITextField *textField;
+
+@property(nonatomic, strong) NSMutableDictionary *searchResults;
+@property(nonatomic, strong) NSMutableArray *searches;
+@property(nonatomic, strong) Flickr *flickr;
+
 
 - (IBAction)shareButtonTapped:(id)sendder;
 
@@ -23,6 +31,19 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"bg_cork.png"]];
+    UIImage *navBarImage = [[UIImage imageNamed:@"navbar.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(27, 27, 27, 27)];
+    [self.toolbar setBackgroundImage:navBarImage forToolbarPosition:UIToolbarPositionAny barMetrics:UIBarMetricsDefault];
+    UIImage *shareButtonImage = [[UIImage imageNamed:@"button.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(8, 8, 8, 8)];
+    [self.shareButton setBackgroundImage:shareButtonImage forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+    
+    UIImage *textFieldImage = [[UIImage imageNamed:@"search_field"] resizableImageWithCapInsets:UIEdgeInsetsMake(10, 10, 10, 10)];
+    [self.textField setBackground:textFieldImage];
+    
+    self.searches = [@[]mutableCopy];
+    self.searchResults = [@{}mutableCopy];
+    self.flickr = [[Flickr alloc] init];
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -36,5 +57,28 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - UITextFieldDelegate methods
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [self.flickr searchFlickrForTerm:textField.text completionBlock:^(NSString *searchTerm, NSArray *results, NSError *error) {
+        if (results && [results count] > 0) {
+            if (![self.searches containsObject:searchTerm]) {
+                NSLog(@"Found %d photos matching %@",[results count],searchTerm);
+                [self.searches insertObject:searchTerm atIndex:0];
+                self.searchResults[searchTerm] = results;
+            }
+            // refresh UI
+            dispatch_async(dispatch_get_main_queue(), ^{
+                
+            });
+        } else {
+            NSLog(@"Error searching Flickr: %@",error.localizedDescription);
+        }
+    }];
+    [textField resignFirstResponder];
+    return YES;
+}
+
 
 @end
